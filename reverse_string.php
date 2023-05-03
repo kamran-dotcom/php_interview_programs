b5f9de14f8a25a86c9ed9eae931daca241a5b6c8
kamran-dotcom <kamichannar2@gmail.com>
2023-05-03 11:06:46 +0500

    reverse string by using buit-in function

--- reverse_string.php
+++ reverse_string.php
@@ -1,4 +1,5 @@
 <?php
+echo "<h5>Reverse by using loop</h5>";
 $name = "kamran";
 
 $len = strlen($name);
@@ -7,4 +8,11 @@ for($i = $len-1; $i >=0; $i--)
 {
     echo $name[$i];
 }
+
+echo "<h5>Reverse by using buit-in-function</h5>";
+
+$name = "kamran";
+$reverse = strrev($name);
+
+echo $reverse;
 ?>
