fd4060cfc102dfe223b0bbc665d334020979ed1d
kamran-dotcom <kamichannar2@gmail.com>
2023-05-25 16:55:54 +0500

    fizzbuzz code

---
+++ new_program.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum($arr) {
+    // Write your code here
+    $len = count($arr);
+
+}
+
+// $arr_temp = rtrim(fgets(STDIN));
+
+// $arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+$arr = [1,3,5,7,9];
+miniMaxSum($arr);
